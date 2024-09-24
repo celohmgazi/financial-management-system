f88ba6bb0ab65077025e181332749d00cb9ee64b
khlongwa023 <khlongwa023@student.wethinkcode.co.za>
2024-09-24 10:06:31 +0200

    wip: dai, creating tables

--- src/main/java/database/DataAccessInterface.java
+++ src/main/java/database/DataAccessInterface.java
@@ -1,5 +1,25 @@
 package database;
 
-public class DataAccessInterface {
+import net.lemnik.eodsql.BaseQuery;
+import net.lemnik.eodsql.Update;
+
+public interface DataAccessInterface extends BaseQuery{
     
+    @Update("CREAT TABLE IF NOT EXISTS users (")
+    public void createUsersTable();
+
+    @Update("CREAT TABLE IF NOT EXISTS income (")
+    public void createIncomeTable();
+
+    @Update("CREAT TABLE IF NOT EXISTS expenses (")
+    public void createExpensesTable();
+
+    @Update("CREAT TABLE IF NOT EXISTS savings (")
+    public void createSavingsTable();
+
+    @Update("CREAT TABLE IF NOT EXISTS budget (")
+    public void createBudgetTable();
+
+    @Update("CREAT TABLE IF NOT EXISTS transactions (")
+    public void createTransactionsTable();
 }
