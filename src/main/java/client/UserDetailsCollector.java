05483b182ca5fc956bcbd513427ef0814367062b
q. ntshangase <qntshangase023@student.wethinkcode.co.za>
2024-09-27 12:34:55 +0200

    wip : first and last name validation

--- src/main/java/client/UserDetailsCollector.java
+++ src/main/java/client/UserDetailsCollector.java
@@ -7,7 +7,7 @@ public class UserDetailsCollector {
     private String firstName;
     private String lastName;
     private String email;
-    private UserDetailsValidator validator;
+    private final UserDetailsValidator validator;
 
     public UserDetailsCollector() {
 
@@ -18,35 +18,48 @@ public class UserDetailsCollector {
     public void collectUserInformation() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your first name: ");
-        this.firstName = scanner.nextLine();
-
-        System.out.println("Enter your last name: ");
-        this.lastName = scanner.nextLine();
-
-        while(true){
-        System.out.println("Enter your email: ");
-        this.email = scanner.nextLine();
-
-        if(validator.isValidEmail(email)){
-            break;
-        }else {
-            System.out.println("Invalid email. Please enter a valid email");
+        while (true) {
+            System.out.println("Enter your first name: ");
+            this.firstName = scanner.nextLine();
+            if (validator.isValidName(firstName)) {
+                break;
+            } else {
+                System.out.println("Invalid first name. Please enter a valid name");
+            }
         }
 
+        while (true) {
+            System.out.println("Enter your last name: ");
+            this.lastName = scanner.nextLine();
+            if (validator.isValidName(lastName)) {
+                break;
+            } else {
+                System.out.println("Invalid last name. Please enter a valid  last name");
+            }
+        }
+
+        while (true) {
+            System.out.println("Enter your email: ");
+            this.email = scanner.nextLine();
+
+            if (validator.isValidEmail(email)) {
+                break;
+            } else {
+                System.out.println("Invalid email. Please enter a valid email");
+            }
+
+        }
     }
 
-    }
+        public String getEmail () {
+            return email;
+        }
 
-    public String getEmail() {
-        return email;
-    }
+        public String getFirstName () {
+            return firstName;
+        }
 
-    public String getFirstName() {
-        return firstName;
+        public String getLastName () {
+            return lastName;
+        }
     }
-
-    public String getLastName() {
-        return lastName;
-    }
-}
