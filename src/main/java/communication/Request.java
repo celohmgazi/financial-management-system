356aed981117908de0c40d0c6b7ef50cbf1de442
sdube023 <sdube023@student.wethinkcode.co.za>
2024-09-27 14:58:22 +0200

    Added methods handlerequests and login to this class to handle the clients request

--- src/main/java/communication/Request.java
+++ src/main/java/communication/Request.java
@@ -5,18 +5,40 @@ import kong.unirest.json.JSONObject;
 
 public class Request {
     
-    public String register(UserDetailsCollector collector) {
+    public String register(String firstName, String lastName, String email) {
         JSONObject personalDetails = new JSONObject();
         JSONObject registrationData = new JSONObject();
 
-        personalDetails.put("firstname", collector.getFirstName());
-        personalDetails.put("lastname", collector.getLastName());
-        personalDetails.put("email", collector.getEmail());
+        personalDetails.put("firstname", firstName);
+        personalDetails.put("lastname", lastName);
+        personalDetails.put("email", email);
 
         registrationData.put("action", "register");
 
         registrationData.put("data", personalDetails);
 
         return registrationData.toString();
+    }
+
+    public String login(String email, String password) {
+        JSONObject loginDetails = new JSONObject();
+        JSONObject loginData = new JSONObject();
+
+        loginDetails.put("email", email);
+        loginDetails.put("password", password);
+
+
+        loginData.put("action", "login");
+
+        loginData.put("data", loginDetails);
+
+        return loginData.toString();
+    }
+
+    public String handleRequests(String request) {
+        if (request.contains("register")) {
+            return register("","","");
+        }
+        return null;
     }
 }
