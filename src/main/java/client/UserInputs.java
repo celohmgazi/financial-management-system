ff10191396851631c2243fc8e659aba94a908f18
sdube023 <sdube023@student.wethinkcode.co.za>
2024-09-27 14:55:52 +0200

    This class was added so that it handles all the users input

--- src/main/java/client/UserInputs.java
+++ src/main/java/client/UserInputs.java
@@ -1,4 +1,95 @@
 package client;
 
+import communication.Request;
+
+import java.util.Scanner;
+
 public class UserInputs {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private final UserDetailsValidator validator;
+    private Request request;
+
+    public UserInputs() {
+        validator = new UserDetailsValidator();
+        request = new Request();
+    }
+
+    public String registrationInfo() {
+        String firstName = getUserFirstName();
+        String lastName = getUserLastName();
+        String email = getUserEmail();
+
+        return request.register(firstName, lastName, email);
+    }
+
+    public String loginInfo() {
+        String email = getUserEmail();
+        String password = getUserPassword();
+
+        return request.login(email, password);
+    }
+
+    public String userCommands() {
+        return "";
+    }
+
+    public String getUserFirstName() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter your first name: ");
+            this.firstName = scanner.nextLine();
+            if (validator.isValidName(firstName)) {
+                return firstName;
+            } else {
+                System.out.println("Invalid first name. Please enter a valid name");
+            }
+        }
+    }
+
+    public String getUserLastName() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter your last name: ");
+            this.lastName = scanner.nextLine();
+            if (validator.isValidName(lastName)) {
+                return lastName;
+            } else {
+                System.out.println("Invalid last name. Please enter a valid  last name");
+            }
+        }
+    }
+
+    public String getUserEmail() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter your email: ");
+            this.email = scanner.nextLine();
+
+            if (validator.isValidEmail(email)) {
+                return email;
+            } else {
+                System.out.println("Invalid email. Please enter a valid email");
+            }
+        }
+    }
+
+    public String getUserPassword() {
+        return null;
+    }
+
+    public String handleUserInputs(String userInput) {
+        if (userInput.equals("register")) {
+            return registrationInfo();
+        } else if (userInput.equals("login")) {
+            return loginInfo();
+        } else {
+            return userCommands();
+        }
+    }
 }
